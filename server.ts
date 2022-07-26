94caf1ad37661664acd86670e30b88e1c823afb1
fcaramez <francisco.caramez.duarte@gmail.com>
2022-07-26 12:51:22 +0100

    release of new version

--- server.ts
+++
@@ -1,8 +0,0 @@
-
-import app from "./app";
-const PORT = process.env.PORT || 5005;
-
-app.listen(PORT, () => {
-  console.log("Server listening on PORT", PORT);
-});
-
--- utils/consts.ts
+++
@@ -1,5 +0,0 @@
-
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/server"
-
-export default MONGO_URI;
-
